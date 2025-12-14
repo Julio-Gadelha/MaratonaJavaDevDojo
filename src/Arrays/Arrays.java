e15f90ee33a22c49c05523e30d5c88439a4f135c
julio gabriel <juliogabriel78956@gmail.com>
2025-12-14 17:02:14 -0300

    ArrayMultidimencionais

---
+++ src/Arrays/Arrays.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class Arrays {
+   public static void main(String[] args) {
+       String [] nomes= new String[4];
+       nomes[0] = "Goku";
+       nomes[1] = "Picollo";
+       nomes[2] = "Vegetta";
+       nomes[3] = "Pateta";
+       for (int i = 0; i < nomes.length; i++) {
+           System.out.println(nomes[i]);
+       }
+    }
+}
---
+++ src/Arrays/ArraysIdade.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class ArraysIdade {
+   public static void main(String[] args) {
+        int[] idade = new  int[3];
+       System.out.println(idade);
+       idade[0] = 21;
+       idade[1] = 23;
+       idade[2] = 25;
+       System.out.println(idade[0]);
+       System.out.println(idade[1]);
+       System.out.println(idade[2]);
+
+
+
+
+
+   }
+}
